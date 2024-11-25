48a6e801a3fc77998766902dcaf44f98f3a59c79
italorochaj <italorochaj@icloud.com>
2024-11-25 02:37:04 -0300

    Criação dos packages, e start das classes iniciais para teste get via insomnia.

---
+++ src/main/java/br/com/calculos/calculos/app/entity/Entrada.java
@@ -0,0 +1,17 @@
+package br.com.calculos.calculos.app.entity;
+
+import javax.swing.event.ListDataEvent;
+import java.util.List;
+
+public class Entrada {
+
+    private List<Integer> lista;
+
+    public List<Integer> getLista() {
+        return lista;
+    }
+
+    public void setLista(List<Integer> lista) {
+        this.lista = lista;
+    }
+}
